49f281829619f617f57981adb0f3af22b9351eef
LuisSilva11 <lssilva07@misena.edu.co>
2022-11-02 21:05:49 -0500

    funcionalidad y diseño

--- js/carasello.js
+++ js/carasello.js
@@ -1,49 +1,46 @@
-let carasello, op, apuesta, pierdes
-let dinero=0, total=0, contador=0 
-let num=1 + (int)(Math.random() * 2)
-dinero = parseInt(dinero)
-total = parseInt(total)
-contador = parseInt(contador)
-num = parseInt(num)
-function juego() {
+let respuesta = document.getElementById("aws")
+let boton = document.getElementById("jugar")
+let botonSalir = document.getElementById("salir")
+let contador = 0
+let total = 0
+let continuar = true
+
+function carasello() {
+
+    let dinero = document.getElementById("diner").value
+    dinero = parseInt(dinero)
+    let moneda = document.getElementById("sele").value
+    moneda = parseInt(moneda)
+    let aleatorio = 1 + parseInt((Math.random() * 2))
+
     do {
-
-        apuesta=prompt("Digite la cantidad que desea apostar: ")
-        alert("El dinero que va a apostar es: " + apuesta);
-
-        carasello=prompt("Digite cara o sello");
-
-        if (carasello.equalsIgnoreCase("cara") && num == 1) {
-            alert("Has ganado");
-            alert("Salió cara");
-            dinero = dinero + apuesta;
-            contador = contador + 1;
-            System.out.println("El dinero que tiene en total es: " + dinero);
-            System.out.println("La cantidad de veces que apostaste fueron: " + contador);
-        }
-
-        else if (carasello.equalsIgnoreCase("sello") && num == 2) {
-            System.out.println("Has ganado :)");
-            System.out.println("Salió sello");
-            dinero = dinero + apuesta;
-            contador = contador + 1;
-            System.out.println("El dinero que tiene en total es: " + dinero);
-            System.out.println("La cantidad de veces que apostaste fueron: " + contador);
+        
+        if (moneda == aleatorio) {
+            contador = contador + 1
+            total += dinero + dinero
+            respuesta.innerHTML = `<p>Has ganado la cantidad de dinero que has acomulado es de ${total}</p>`
         }
 
         else {
-            pierdes = total - apuesta;
-            System.out.println("Has perdido :( " + pierdes);
-            dinero = dinero - apuesta;
-            contador = contador + 1;
-            System.out.println("El dinero que tiene en total es: " + dinero);
-            System.out.println("La cantidad de veces que apostaste fueron: " + contador);
-
+            contador = contador + 1
+            total = dinero - dinero
+            respuesta.innerHTML = `<p>Has perdido la cantidad de dinero que llevas en este momento es ${total}</p>`
         }
 
+        continuar = false
 
-        System.out.println("Si quieres volver a apostar escribe si, de lo contrario escribe no");
-        op = lectura.nextLine();
+    }
 
-    } while (op.equalsIgnoreCase("si"));
+    while (continuar != false)
+
 }
+
+boton.addEventListener('click', carasello)
+
+function salida() {
+
+    respuesta.innerHTML = `<p> El total de dinero ganado fue ${total} en ${contador} partidas</p>`
+
+}
+
+botonSalir.addEventListener('click', salida)
